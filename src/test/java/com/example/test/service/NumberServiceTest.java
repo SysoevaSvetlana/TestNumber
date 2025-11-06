df6d46ecb38cdbe4c224d5e629d10f0c91ae4d14
SvetlanaSysoeva03 <dorri.ribka@yandex.ru>
2025-11-06 22:39:48 +0300

    Fix unit tests - initialize NumberService with constructor instead of @InjectMocks

--- src/test/java/com/example/test/service/NumberServiceTest.java
+++ src/test/java/com/example/test/service/NumberServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -23,8 +22,12 @@ class NumberServiceTest {
     @Mock
     private NumberRepository numberRepository;
 
-    @InjectMocks
     private NumberService numberService;
+
+    @BeforeEach
+    void setUp() {
+        numberService = new NumberService(numberRepository);
+    }
 
     @Test
     void addNumberAndGetSorted_shouldSaveNumberAndReturnSortedList() {
